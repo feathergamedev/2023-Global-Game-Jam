eb5df1a3c2062206f798547472191313bef17148
decade8788 <108680849+decade8788@users.noreply.github.com>
2023-02-04 14:49:02 +0800

    new

--- Assets/Game/Scripts/UI/Water.cs
+++ Assets/Game/Scripts/UI/Water.cs
@@ -6,12 +6,40 @@ using UnityEngine.UI;
 public class Water : MonoBehaviour
 {
     public Water water;
-    public Fillimgae fillimgae;
+    public Image fillimgae;
     private Slider slider;
 
     void Awake()
     {
         slider = GetComponent<Slider>();
+
+        var tracker = new ResourceTracker(100, 100, 100);
+        tracker.IncreaseWater(10);
+        tracker.DecreaseWater(5);
+
+        tracker.ResourceValueChanged += (sender, args) =>
+        {
+            switch (args.Value)
+            {
+                case WaterResource w:
+                    // Update value to UI or somewhere
+                    // args.Value;
+                    if (args.Type == ResourceValueChangedEventArgs.ChangeType.Increase)
+                    {
+                        slider.value = w.Value;
+                        // do something 
+                    }
+                    else if (args.Type == ResourceValueChangedEventArgs.ChangeType.Decrease)
+                    {
+                        // do something 
+                    }
+                    break;
+                case TimeResource t:
+                    break;
+                //default:
+                   // throw new ArgumentOutOfRangeException();
+            }
+        };
     }
 
     // Update is called once per frame
@@ -22,11 +50,11 @@ public class Water : MonoBehaviour
             fillimgae.enabled = false;
         }
 
-        if (slider.value > slider.maxValue && !fillimgae.enbled)
+        if (slider.value > slider.maxValue && !fillimgae.enabled)
         {
             fillimgae.enabled = true;
         }
-        //float fillvalue = PlayerWater.current / playerWater.max;
-        slider.value = fillvalue;
+        // float fillvalue = 
+        //slider.value = fillvalue;
     }
 }
